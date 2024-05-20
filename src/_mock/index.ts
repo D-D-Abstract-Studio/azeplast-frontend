5b2b91c791da17e9a35945351b161f6f9b75b6b5
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-20 01:19:49 -0300

    remove unused mock data files
    The commit removes the unused mock data files for files, invoice, and overview. These files were not being used in the project and were taking up unnecessary space. The commit also updates the index file to remove the exports for these unused files.

--- src/_mock/index.ts
+++ src/_mock/index.ts
@@ -1,11 +1,6 @@
 export * from './assets';
 export * from './_mock';
 
-// ----------------------------------------------------------------------
-
 export * from './_user';
-export * from './_files';
 export * from './_order';
 export * from './_others';
-export * from './_invoice';
-export * from './_overview';
