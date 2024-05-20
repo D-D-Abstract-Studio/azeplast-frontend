974560575741c7800e444ab9834b2f6f9b8263f1
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-20 06:05:53 -0300

    refactor: Update Vite configuration, add TypeScript configuration, and remove unused code and files

---
+++ src/app/index.tsx
@@ -0,0 +1,57 @@
+import 'simplebar-react/dist/simplebar.min.css'
+
+import 'yet-another-react-lightbox/styles.css'
+import 'yet-another-react-lightbox/plugins/captions.css'
+import 'yet-another-react-lightbox/plugins/thumbnails.css'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+import 'react-quill/dist/quill.snow.css'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import 'react-lazy-load-image-component/src/effects/blur.css'
+
+import ThemeProvider from '../theme'
+import { primaryFont } from '../theme/typography'
+
+import ProgressBar from '../components/progress-bar'
+import { MotionLazy } from '../components/animate/motion-lazy'
+import SnackbarProvider from '../components/snackbar/snackbar-provider'
+import { SettingsProvider, SettingsDrawer } from '../components/settings'
+import { AuthProvider, AuthConsumer } from '../auth/context/jwt'
+import { KanbanView } from '../sections/kanban/view'
+
+export const App = () => {
+  return (
+    <html lang="en" className={primaryFont.className}>
+      <body>
+        <AuthProvider>
+          <SettingsProvider
+            defaultSettings={{
+              themeMode: 'light',
+              themeDirection: 'ltr',
+              themeContrast: 'default',
+              themeLayout: 'vertical',
+              themeColorPresets: 'default',
+              themeStretch: false,
+            }}
+          >
+            <ThemeProvider>
+              <MotionLazy>
+                <SnackbarProvider>
+                  <SettingsDrawer />
+                  <ProgressBar />
+                  <AuthConsumer>
+                    <KanbanView />
+                  </AuthConsumer>
+                </SnackbarProvider>
+              </MotionLazy>
+            </ThemeProvider>
+          </SettingsProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
-};
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
