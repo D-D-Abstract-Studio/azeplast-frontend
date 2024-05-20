fd3e52c9171b67dedd2667fc9b5648c6aa3b5be2
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-20 01:49:56 -0300

    refactor: Remove unused mock data files and exports
    This commit removes the unused mock data files for files, invoice, overview, job, tour, blog, product, and calendar. These files were not being used in the project and were taking up unnecessary space. The commit also updates the index file to remove the exports for these unused files.

--- src/_mock/index.ts
+++ src/_mock/index.ts
@@ -1,6 +1,3 @@
 export * from './assets';
 export * from './_mock';
-
-export * from './_user';
-export * from './_order';
 export * from './_others';
--- src/hooks/use-mocked-user.ts
+++ src/hooks/use-mocked-user.ts
@@ -1,17 +1,5 @@
 import { _mock } from 'src/_mock';
 
-// TO GET THE USER FROM THE AUTHCONTEXT, YOU CAN USE
-
-// CHANGE:
-// import { useMockedUser } from 'src/hooks/use-mocked-user';
-// const { user } = useMockedUser();
-
-// TO:
-// import { useAuthContext } from 'src/auth/hooks';
-// const { user } = useAuthContext();
-
-// ----------------------------------------------------------------------
-
 export function useMockedUser() {
   const user = {
     id: '8864c717-587d-472a-929a-8e5f298024da-0',
