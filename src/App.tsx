c8124360634492bbd9e44961240f0a1993956860
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-23 02:48:43 -0300

    refactor: Remove unused imports and files related to authentication, settings, next/navigation, and Vite configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,3 @@
-import 'simplebar-react/dist/simplebar.min.css'
-
-import 'yet-another-react-lightbox/styles.css'
-import 'yet-another-react-lightbox/plugins/captions.css'
-import 'yet-another-react-lightbox/plugins/thumbnails.css'
-
-import 'mapbox-gl/dist/mapbox-gl.css'
-
-import 'react-quill/dist/quill.snow.css'
-
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-
 import 'react-lazy-load-image-component/src/effects/blur.css'
 
 import ThemeProvider from './theme'
